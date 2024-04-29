09e682adf6a91bf0be98b8e4295ddba096a71496
DANILOSILVA1A <167550485+DANILOSILVA1A@users.noreply.github.com>
2024-04-29 09:12:17 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,41 @@
-//preparar e rodar um código
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
 function setup() {
-  createCanvas(400, 200);//criar tela
-  background("pink");
+  createCanvas(400, 400);
+   background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
 }
 
-//responsável por desenhar
+
 function draw() {
-  stroke("blue");
-  fill("red");
   
-  if(mouseIsPressed){
-    rect(mouseX, mouseY, 40, 40);
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,50);
+ 
+  
+  
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
   }
+  
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+  
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+    
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+    
 }
